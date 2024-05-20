bb07615735bfbd94a9fcd9438e1f286af09a87e4
Darién Julián Ramírez <ianshalaga@gmail.com>
2024-05-20 10:49:16 +0200

    Ticket service and controller created

--- src/dao/factory.ts
+++ src/dao/factory.ts
@@ -1,13 +1,16 @@
+/** Config */
 import config from "../config/env.config";
 import MongoDB from "../config/mongoDb.config";
 /** DAO */
 import CartMongodbDAO from "./mongodb/cart.mongodb.dao";
 import ProductMongodbDAO from "./mongodb/product.mongodb.dao";
+import TicketMongodbDAO from "./mongodb/ticket.mongodb.dao";
 import UserMongodbDAO from "./mongodb/user.mongodb.dao";
 
 export let carts: CartMongodbDAO;
 export let products: ProductMongodbDAO;
 export let users: UserMongodbDAO;
+export let tickets: TicketMongodbDAO;
 
 switch (config.dao) {
   case "mongodb":
@@ -21,9 +24,13 @@ switch (config.dao) {
     const { default: usersMongodb } = await import(
       "./mongodb/user.mongodb.dao"
     );
+    const { default: ticketsMongodb } = await import(
+      "./mongodb/ticket.mongodb.dao"
+    );
     carts = new cartsMongodb();
     products = new productsMongodb();
     users = new usersMongodb();
+    tickets = new ticketsMongodb();
     break;
   case "files":
     // No implementation
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,8 +1,12 @@
-import { carts, products, users } from "../dao/factory";
+/** Factory */
+import { carts, products, users, tickets } from "../dao/factory";
+/** Services */
 import CartService from "./cart.service";
 import ProductService from "./product.service";
 import UserService from "./user.service";
+import TicketService from "./ticket.service";
 
 export const cartService = new CartService(carts);
 export const productService = new ProductService(products);
 export const userService = new UserService(users);
+export const ticketService = new TicketService(tickets);
